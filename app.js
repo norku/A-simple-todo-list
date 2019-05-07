109cdad3d835460a525e6dfbc08acfb354528726
Noora Kurko <noora.kurko@gmail.com>
2019-05-07 14:55:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,55 @@
-$(document).ready(()=>{
+$(document).ready(() => {
+  function appendToLI() {
+    $("li")
+      .append("Done: " + '<input type="checkbox" class="finished"><br>')
+      .append('<button class="edit">Edit</button>')
+      .append('<button class="delete">Delete</button></li>');
+  }
 
-
-//Add things to the to-do list
-  $('#addThings').submit((e)=>{
+  const itemList = [];
+  //Add things to the to-do list
+  $("#addThings").submit(e => {
     e.preventDefault();
-    const $text = $('input').val();
-    if ($text == '') {
+
+    // const $text = $('input').val();
+
+    //   if ($text == '') {
+    //     $("#errorMessage").slideDown(200, function() {
+    //       //if text field is empty an error message slides down, delays and slides up
+    //       $("#errorMessage").text("That field can't be submitted empty!")
+    //                         .delay(3000)
+    //                         .slideUp(300);
+    //     });
+    //   } else {
+    //   $('input').val('');
+    //  $("ul").append('<li class="item">'  + $text + '<br>');
+    //   itemList.push($text);
+    //   appendToLI();
+    //   }
+    //   console.log(itemList);
+    // });
+
+    const $text = $("input").val();
+    if ($text == "") {
       $("#errorMessage").slideDown(200, function() {
-        //Stuff to do *after* the animation takes place
-        $("#errorMessage").text("That field can't be submitted empty!")
-                          .delay(3000)
-                          .slideUp(300);
+        //if text field is empty an error message slides down, delays and slides up
+        $("#errorMessage")
+          .text("That field can't be empty!")
+          .delay(3000)
+          .slideUp(300);
       });
     } else {
-    $('input').val('');
-    $("ul").append('<li class="item">'  + $text + '</br>')
-           .append('Done: ' + '<input type="checkbox" class="finished"></li>');
-           if ($('checkbox') === 'chec'
+      $("input").val("");
+      itemList.push($text);
     }
+    console.log(itemList);
   });
 
-  const checkbox = 'Done: ' + '<input type="checkbox" class="finished">';
-
+  if (itemList.length > 0) {
+    for (let i = 0; i < itemList.length; i++) {
+      $(".main").append(itemList[i]);
+      // $('ul').append('<li class="item">' + itemList[i] + '<br>');
+      // appendToLI();
+    }
+  }
 });
