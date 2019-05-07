06635153113522f15913f5344eca41e3d0e70e1d
Noora Kurko <noora.kurko@gmail.com>
2019-05-07 12:37:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
 $(document).ready(()=>{
 
-  const savingFields = {};
 
-  function appendToLI(elementName, property, value) {
-      const element = $('li').append(elementName);
-      element[property] = value;
-      return element;
-  }
-
-  function handleSubmit() {
-    const handleFormSubmit = ((e) => {
-      e.preventDefault();
-      const date = {};
-      const theList =
-    });
-  }
-
+//Add things to the to-do list
   $('#addThings').submit((e)=>{
-    console.log( $('input').serializeArray());
     e.preventDefault();
-    const text = $('input').val().toString();
+    const $text = $('input').val();
+    if ($text == '') {
+      $("#errorMessage").slideDown(200, function() {
+        //Stuff to do *after* the animation takes place
+        $("#errorMessage").text("That field can't be submitted empty!")
+                          .delay(3000)
+                          .slideUp(300);
+      });
+    } else {
     $('input').val('');
-    // $("ul").append("<li></li>").text(text);
+    $("ul").append('<li class="item">'  + $text + '</br>')
+           .append('Done: ' + '<input type="checkbox" class="finished"></li>');
+           if ($('checkbox') === 'chec'
+    }
   });
 
-
+  const checkbox = 'Done: ' + '<input type="checkbox" class="finished">';
 
 });
